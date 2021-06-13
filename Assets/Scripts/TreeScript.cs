73b592b3285df495529d1252ef7074d876d67a2d
siberianbearofficial <alexeyorlov65@gmail.com>
2021-06-13 20:08:28 +0300

    Particles

--- Assets/Scripts/TreeScript.cs
+++ Assets/Scripts/TreeScript.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class TreeScript : MonoBehaviour
 {
     public Animator animator;
+    public GameObject fireEffect;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "fireboy")
+        {
             animator.Play("TreeAnim");
+            fireEffect.SetActive(true);
+            StartCoroutine(wait());
+        }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(8);
+        fireEffect.SetActive(false);
+        gameObject.SetActive(false);
     }
 }
